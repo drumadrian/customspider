59e3b64f7e3d76ebef93ae473b220b8f1ca0d383
EC2 Default User <ec2-user@ip-172-31-28-72.us-west-2.compute.internal>
2016-08-08 10:30:13 +0000

    updated core script

--- shell_version_1/spider_1.bash
+++ shell_version_1/spider_1.bash
@@ -3,24 +3,28 @@
 echo "START Script"
 
 
-echo "Creating folder for $1"
-mkdir $1 
+#echo "Creating folder for $1"
+#output_directory=spider_1_output/$1
+output_directory=spider_1_output
+echo "output_directory = $output_directory" 
+#mkdir $output_directory
 
-echo "entering folder $1"
-cd $1
+echo "entering folder $output_directory"
+cd $output_directory
 
 #use these commands on Linux
 #echo "Running wget -r on $1"
 #wget -r $1 > $1.output 
+#echo "Running:      wget --random-wait -r -p -e robots=off -U mozilla on $1"
+#wget --random-wait -r -p -e robots=off -U mozilla $1 
 
 
-#use these commands on OSX
-echo "Running curl $1 -o $1.output"
-curl "$1" -o $1.output 
+echo "Running:    wget --random-wait --mirror --page-requisites --domains=$1 -e --user-agent=AGENT mozilla $1"
+#Example: 
+#wget --random-wait --mirror --page-requisites --domains=extremenetworks.com -e --user-agent=AGENT mozilla www.extremenetworks.com
+wget --random-wait --mirror --page-requisites --domains=$1 -e --user-agent=mozilla $1
 
 
 
 
-
-
-echo "FINISH Script"
+echo "FINISHED Script for $1"
---
+++ shell_version_1/spider_1_output/README.md
@@ -0,0 +1,1 @@
+Output files for spider_1.bash are written to this directory by default.   
